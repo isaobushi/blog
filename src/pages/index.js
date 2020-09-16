9577f1fbb52c2da6d277a39bd1602c3f45a4cfa7
Andrea <andrea.mele.au@gmail.com>
2020-09-16 16:35:39 +1000

    QUALITY: disable hex

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ export default ({ location, data }) => {
   const path = location.pathname ? location.pathname : "";
   return (
     <Layout path={path}>
-      <HexSVG />
+      {/* <HexSVG /> */}
       <Item />
       <H2 variant="default">Latest posts</H2>
       <hr style={{ borderColor: dark ? color.accent : color.neutral100 }} />
